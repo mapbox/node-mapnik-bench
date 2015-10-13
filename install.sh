ff6ddc9d1f4b78a0ebabab207d184647fe4cde26
Dane Springmeyer <dane@mapbox.com>
2015-10-13 16:56:18 -0700

    install mapnik-pool after

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# install basic node deps
-npm install
-# remove the mapnik installed as a peer of mapnik-pool
-rm -rf node_modules/mapnik
-
 # now we install mapnik+tilelive-bridge+tilelive pairs that work(ed) together in the past
 # we're interested in when the mapnik version major or minor was bumped
 # https://github.com/mapbox/tilelive-bridge/releases
@@ -20,3 +15,8 @@ rm -rf node_modules/mapnik
 
 # https://github.com/mapbox/tilelive-bridge/blob/v2.1.0/package.json#L16
 (cd mapnik-versions/v3.4.9 && npm install && npm ls mapnik tilelive-bridge)
+
+# install basic node deps
+npm install
+# remove the mapnik installed as a peer of mapnik-pool
+rm -rf node_modules/mapnik
