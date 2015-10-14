c98282ed53e266875e23c455fae1c05a571a1330
Dane Springmeyer <dane@mapbox.com>
2015-10-13 21:41:18 -0700

    echo test

--- bench.sh
+++ bench.sh
@@ -7,6 +7,8 @@ NODE_VERSION=${NODE_VERSION:-"0.10"}
 TESTCASE=${TESTCASE:-"./testcases/L7"}
 OPTIONS=${OPTIONS:="--null-bridge --threadpool=8"}
 
+echo "Testing '${TESTCASE}' with '${OPTIONS}'"
+
 if [[ ${NODE_VERSION} != "4" ]]; then
     node test.js ${TESTCASE}/map.xml ${OPTIONS} mapnik-versions/v3.2.0/;
 fi
