20ef1ad62eca211b0f5d44fd8658bea160ce70fe
dnomadb <damon@mapbox.com>
2016-01-07 10:54:46 -0800

    test cases to look for shift

---
+++ testcases/resample_lower_nearest/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${CURRENT_DIR}
+
+FILENAME=lowres.tif
+
+if [[ ! -f ./${FILENAME} ]]; then
+    wget https://s3.amazonaws.com/mapbox/playground/dnomadb/gdal-tiling-bench/${FILENAME}
+else
+    echo "done: already downloaded ${FILENAME}"
+fi
