c510d8d6170f3b60cffc5a67e85fa36b5434594d
mapsam <matthews.sam@gmail.com>
2016-02-01 13:39:24 -0800

    run new versions

--- test-all.sh
+++ test-all.sh
@@ -11,10 +11,12 @@ echo '{ "versions": {} }' >> ${DATA_FILE}
 TESTCASE=/Users/mapsam/mapbox/gdal-tiling-bench/testcases/geojson/*
 OPTIONS=${OPTIONS:="--threadpool=8"}
 
-for v in v2_spec latest
+for v in v2_spec latest v3.4.9 v3.4.0
 do
+	echo "RUNNING ${v}"
 	for t in ${TESTCASE}
 	do
+		echo "> against ${t##*/map.xml}"
 		node test.js ${t}/map.xml ${OPTIONS} mapnik-versions/${v} --json
 	done
 done
