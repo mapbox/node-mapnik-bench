a0cdcf977eea7cd3b267a6e5b364ce6d65e3df2f
bergwerkgis <wb@BergWerk-GIS.at>
2016-04-19 16:42:28 -0700

    add 512x512 test cases

---
+++ testcases/512-lzw-none.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  geojson: [],
+  tif: [
+    {
+      name: '13-1847-3643_512_lzw',
+      path: path.join(__dirname, 'tif', '13-1847-3643_512_lzw.tif')
+    },
+    {
+      name: '13-1847-3649_512_lzw',
+      path: path.join(__dirname, 'tif', '13-1847-3649_512_lzw.tif')
+    },
+    {
+      name: '13-1847-3643_512_none',
+      path: path.join(__dirname, 'tif', '13-1847-3643_512_none.tif')
+    },
+    {
+      name: '13-1847-3649_512_none',
+      path: path.join(__dirname, 'tif', '13-1847-3649_512_none.tif')
+    }
+  ]
+};
